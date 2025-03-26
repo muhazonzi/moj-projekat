9099771b6423235848e0c099c020080897fe356a
muhazonzi <eeem090@gmail.com>
2025-03-26 16:45:06 +0100

    Nove promjene 5

---
+++ src/main/java/org/acme/resource/KorisnikResource.java
@@ -0,0 +1,24 @@
+package org.acme.resource;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.model.Korisnik;
+import org.acme.repository.KorisnikSource;
+
+public class KorisnikResource {
+
+    @Inject
+    KorisnikSource KorisnikSource;
+
+    @Path("dodajKorisnika")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response dodajKorisnika(Korisnik k) {
+        Korisnik korisnik = KorisnikSource.dodajKorisnika(k);
+        return Response.ok(korisnik).build();
+    }
+}
